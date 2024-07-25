a7579bcd56c461ba84eac2fbd38674c3de1dfad5
Mwan3kii <agathawanja303@gmail.com>
2024-07-25 17:31:03 +0300

    Change variable

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Returns a list of lists of intergers rep Pascals triangle"""
+
+
 def pascal_triangle(n):
     """Returns an empty list if n <= 0"""
     if n <= 0:
@@ -7,8 +9,8 @@ def pascal_triangle(n):
     triangle = [[1]]
     for i in range(1, n):
         row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        for k in range(1, i):
+            row.append(triangle[i - 1][k - 1] + triangle[i - 1][k])
         row.append(1)
         triangle.append(row)
     return triangle
