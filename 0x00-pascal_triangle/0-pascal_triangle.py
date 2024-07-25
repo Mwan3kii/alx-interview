b69c815ec31d7231009ed549ba752739b2486d3f
Mwan3kii <agathawanja303@gmail.com>
2024-07-25 20:44:14 +0300

    Empty list

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,7 +16,7 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    triangle = [[1]]
+    triangle = []
     if n > 0:
         for i in range(1, n + 1):
             level = []
