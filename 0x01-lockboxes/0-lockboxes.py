d99478b511c2c3c0db9787fbce1e43e77496f758
Mwan3kii <agathawanja303@gmail.com>
2024-07-30 18:27:04 +0300

    Fix pycodestyle error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """Method that determines if all the boxes can be opened."""
+
+
 def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be unlocked.
+
+    Args:
+        boxes (list of list of int): Each inner list has keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, otherwise False.
+    """
     n = len(boxes)
     unlocked = [False] * n
     unlocked[0] = True
     queue = [0]
-    
+
     while queue:
         current_box = queue.pop(0)
         for key in boxes[current_box]:
