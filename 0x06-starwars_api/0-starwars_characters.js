10cb6593ef00e5d5e746a0f96f888a45e3044267
Mwan3kii <agathawanja303@gmail.com>
2024-09-05 22:14:48 +0300

    Fix errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-
-// Get the Movie ID from the command line argument
 const movieId = process.argv[2];
 
 if (!movieId) {
   console.log('Usage: ./0-starwars_characters.js <movie_id>');
   process.exit(1);
 }
-
-// Construct the URL to fetch movie details
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Make a request to get the movie details
 request(url, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
@@ -23,8 +17,6 @@ request(url, (error, response, body) => {
   if (response.statusCode === 200) {
     const movieData = JSON.parse(body);
     const characters = movieData.characters;
-
-    // Fetch each character's details and print the name
     characters.forEach(characterUrl => {
       request(characterUrl, (charError, charResponse, charBody) => {
         if (charError) {
