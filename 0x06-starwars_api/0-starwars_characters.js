20f928489ce38554a77f1f4e243ccb145216d8e2
Mwan3kii <agathawanja303@gmail.com>
2024-09-05 21:35:29 +0300

    Try method for ouput

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,30 @@
 #!/usr/bin/node
 
 const axios = require('axios');
-
 const movieId = process.argv[2];
-if (!movieId) {
-  console.log('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-axios.get(url)
-  .then(response => {
-    const characters = response.data.characters;
-    characters.forEach(characterUrl => {
-      axios.get(characterUrl)
-        .then(characterResponse => {
-          console.log(characterResponse.data.name);
-        })
-        .catch(error => {
-          console.log(`Error fetching character: ${error.message}`);
-        });
-    });
-  })
-  .catch(error => {
-    console.log(`Error fetching movie: ${error.message}`);
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  axios(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
   });
+}
+
+axios(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
+});
