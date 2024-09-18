9281670d33d2ebcec37d678cfa2bae7a6d517519
Mwan3kii <agathawanja303@gmail.com>
2024-09-18 21:45:33 +0300

    Test effficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,13 +6,13 @@ def makeChange(coins, total):
     """Return fewest no of coins needed to meet total"""
     if total <= 0:
         return 0
-    current_total = 0
+    curr_total = 0
     used_coins = 0
     coins = sorted(coins, reverse=True)
     for coin in coins:
-        r = (total - current_total) // coin
-        current_total += r * coin
-        used_coins += r
-        if current_total == total:
+        dp = (total - curr_total) // coin
+        curr_total += dp * coin
+        used_coins += dp
+        if curr_total == total:
             return used_coins
     return -1
