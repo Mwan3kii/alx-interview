5b1891bdb490342a6c80179d7fd458f87a6aa179
Mwan3kii <agathawanja303@gmail.com>
2024-09-18 21:34:17 +0300

    Test effficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,12 +6,8 @@ def makeChange(coins, total):
     """Return fewest no of coins needed to meet total"""
     if total <= 0:
         return 0
-    coins.sort(reverse=True)
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    dp = [0] + [float("inf")] * (total)
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
-            if dp[total] != float('inf'):
-                return dp[total]
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[-1] if dp[-1] != float("inf") else -1
