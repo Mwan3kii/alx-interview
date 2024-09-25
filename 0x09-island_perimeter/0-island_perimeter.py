b3ac6c29555918ce243f859f9da042b0562bb73e
Mwan3kii <agathawanja303@gmail.com>
2024-09-25 19:26:43 +0300

    Test island grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
 """Defines function to create island perimeter"""
+
+
 def island_perimeter(grid):
     """Returns perimeter of island grid"""
     perimeter = 0
     rows = len(grid)
     cols = len(grid[0]) if rows > 0 else 0
-
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:  # Land cell
-                # Check all four sides
-                # Top side
-                if i == 0 or grid[i - 1][j] == 0:
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                if r == 0 or grid[r - 1][c] == 0:
                     perimeter += 1
-                # Bottom side
-                if i == rows - 1 or grid[i + 1][j] == 0:
+                if r == rows - 1 or grid[r + 1][c] == 0:
                     perimeter += 1
-                # Left side
-                if j == 0 or grid[i][j - 1] == 0:
+                if c == 0 or grid[r][c - 1] == 0:
                     perimeter += 1
-                # Right side
-                if j == cols - 1 or grid[i][j + 1] == 0:
+                if c == cols - 1 or grid[r][c + 1] == 0:
                     perimeter += 1
 
     return perimeter
