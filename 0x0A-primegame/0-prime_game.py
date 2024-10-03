f8e19adf6811938d1cfe600ce2910a136194544d
Mwan3kii <agathawanja303@gmail.com>
2024-10-03 18:16:02 +0300

    Test checker ouput

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,31 +6,31 @@ def isWinner(x, nums):
     """Determine winner of game each round"""
     if not nums or x <= 0:
         return None
-    max_n = max(nums)
-    primes = [True] * (max_n + 1)
+    max_num = max(nums)
+    primes = [True] * (max_num + 1)
     primes[0] = primes[1] = False
 
-    for i in range(2, int(max_n ** 0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, max_n + 1, i):
-                primes[j] = False
+    for m in range(2, int(max_num ** 0.5) + 1):
+        if primes[m]:
+            for b in range(m * m, max_num + 1, m):
+                primes[b] = False
 
-    prime_count = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+    primes_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        primes_count[i] = primes_count[i - 1] + (1 if primes[i] else 0)
 
-    maria_wins = 0
-    ben_wins = 0
+    maria = 0
+    ben = 0
 
     for n in nums:
-        if prime_count[n] % 2 == 0:
-            ben_wins += 1
+        if primes_count[n] % 2 == 0:
+            ben += 1
         else:
-            maria_wins += 1
+            maria += 1
 
-    if maria_wins > ben_wins:
+    if maria > ben:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif ben > maria:
         return "Ben"
     else:
         return None
